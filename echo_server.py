5f9d63059e3019c5a921a2a2d11788ca1e687527
escaped <none>
2024-05-22 09:50:23 +0200

    updated echo_server.py

--- echo_server.py
+++ echo_server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("localhost", 22222))
@@ -6,6 +6,8 @@ server_socket.listen(10)
 print("Server listen")
 
 messages = []
+encoding = "utf-8"
+
 
 counter = 0
 while counter < 10:
@@ -13,11 +15,11 @@ while counter < 10:
     
     client_socket, client_address = server_socket.accept()
     
-    data = client_socket.recv(1024).decode("utf-8")
+    data = client_socket.recv(1024).decode(encoding)
     
     if data == "get-messages":
         message = "\n".join(messages)
-        client_socket.sendall(message.encode("utf-8"))
+        client_socket.sendall(message.encode(encoding))
     else:
         messages.append(data)
     
